97351bdcab4608270a52e1a522daf69e461d05c7
kyoya0819 <kyoya0819@gmail.com>
2024-08-30 06:53:57 +0900

    APIにPOSTする方式から、APIサーバー経由でリダイレクトする方法に変更
    `/api/press-release/view`を廃止。
    プレスリリース移管前にAPIにPOSTするのではなく、GETでAPIサーバー経由でリダイレクトする方法に変更。

--- backend/app/Http/Controllers/Web/PressRelease/RedirectController.php
+++ backend/app/Http/Controllers/Web/PressRelease/RedirectController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\PressRelease;
+namespace App\Http\Controllers\Web\PressRelease;
 
 use App\Exceptions\HttpJsonResponseException;
 use App\Http\Controllers\Controller;
@@ -9,41 +9,38 @@ use App\UseCases\ViewHistory\UpdateOrCreateAction as ViewHistoryUpdateOrCreateAc
 use App\UseCases\PressRelease\FindByIdsAction;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class ViewController extends Controller
+class RedirectController extends Controller
 {
     /**
+     * @param string $company_id
+     * @param string $release_id
      * @param Request $request
-     * @return JsonResponse
+     * @return RedirectResponse
      * @throws Exception
      */
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(string $company_id, string $release_id, Request $request): RedirectResponse
     {
         $user_id = Auth::id();
         if ($user_id === null || is_string($user_id)) // auth:sanctumで弾いているため最低限の例外
             throw new Exception("想定外エラー");
 
-        $company_id = $request->get("company_id");
-        $release_id = $request->get("release_id");
-
-        $press_release = FindByIdsAction::run(company_id: $company_id, release_id: $release_id);
+        $press_release = FindByIdsAction::run(company_id: (int) $company_id, release_id: (int) $release_id);
 
         if (!$press_release)
-            throw new HttpJsonResponseException(
-                400,
-                "notfound",
-                "PressRelease Not Found",
-                compact("company_id", "release_id")
-            );
+            abort(404);
 
         /* @var Keyword $keyword */
-        foreach ($press_release->keywords() as $keyword) {
+        foreach ($press_release->keywords()->get() as $keyword) {
 
             ViewHistoryUpdateOrCreateAction::run($user_id, $keyword->id);
         }
 
-        return response()->json(["result" => "ok"]);
+        $company_id_str = str_pad($company_id, 9, 0, STR_PAD_LEFT);
+        $release_id_str = str_pad($release_id, 9, 0, STR_PAD_LEFT);
+        return response()->redirectTo("https://prtimes.jp/main/html/rd/p/$release_id_str.$company_id_str.html");
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,7 +12,6 @@ Route::post("/signin/verify", [Auth\SignInController::class, "verify"]);
 
 Route::middleware("auth:sanctum")->group(function () {
 
-    Route::post("/press-release/view", PressRelease\ViewController::class);
     Route::get("/press-release/recommend", PressRelease\RecommendController::class);
 
     Route::get("/company/list", Company\ListController::class);
