41857b9c864bb534d8acf26382c5b1676796d8e2
kikukiku1111111 <k.yudai50view@gmail.com>
2024-08-29 17:18:04 +0900

    loading-fix

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,17 +1,10 @@
-import { Box } from '@mui/material'
 import Image from 'next/image'
 
 const Loading = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div className="flex justify-center items-center min-h-screen">
       <Image src="/loading.svg" width={150} height={150} alt="loading..." />
-    </Box>
+    </div>
   )
 }
 
