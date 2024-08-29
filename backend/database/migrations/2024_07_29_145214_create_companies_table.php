6a5eb9ebd1e440110cee46433b4cdc4791e7f078
kyoya0819 <kyoya0819@gmail.com>
2024-08-30 02:35:49 +0900

    Companyの追加

---
+++ backend/database/migrations/2024_07_29_145214_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("company_id")->unsigned()->unique(); // PR TIMES
+
+            $table->string("name");
+            $table->integer("popularity")->default(0);
+
+            $table->integer("industry_id");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
