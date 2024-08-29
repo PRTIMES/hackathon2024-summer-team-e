35db1b208990462745458bbfbc1d90a83e3c5f29
kyoya0819 <kyoya0819@gmail.com>
2024-08-29 17:17:34 +0900

    PressReleaseをcompany_idとrelease_idをもとに検索するUseCaseの作成

---
+++ backend/app/UseCases/PressRelease/FindByIdsAction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\UseCases\PressRelease;
+
+use App\Models\PressRelease;
+use App\Models\User;
+
+class FindByIdsAction
+{
+
+    /**
+     * @param int $company_id
+     * @param int $release_id
+     * @return User|null
+     */
+    public static function run(
+        int $company_id,
+        int $release_id
+    ): PressRelease|null
+    {
+        return PressRelease::where("company_id", $company_id)
+                           ->where("release_id", $release_id)
+                           ->first();
+    }
+}
