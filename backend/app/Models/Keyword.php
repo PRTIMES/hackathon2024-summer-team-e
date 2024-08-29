7408785e93880e21356575d9b0776f6aa4d47a38
kyoya0819 <kyoya0819@gmail.com>
2024-08-29 17:12:10 +0900

    PressReleaseとKeywordのリレーションを作成

--- backend/app/Models/Keyword.php
+++ backend/app/Models/Keyword.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Keyword extends Model
 {
@@ -13,4 +14,12 @@ class Keyword extends Model
         "keyword",
         "weight"
     ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function pressReleases(): BelongsToMany
+    {
+        return $this->belongsToMany(PressRelease::class);
+    }
 }
--- backend/app/Models/PressRelease.php
+++ backend/app/Models/PressRelease.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class PressRelease extends Model
 {
@@ -16,4 +17,12 @@ class PressRelease extends Model
         "summary",
         "release_created_at"
     ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function keywords(): BelongsToMany
+    {
+        return $this->belongsToMany(Keyword::class);
+    }
 }
